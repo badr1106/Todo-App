1e53efe108af434b5e6f190a22225b87da5a17b0
badr1106 <ahmed.badr_aast@hotmail.com>
2021-10-24 16:32:05 +0200

    Using class components and state instead of functional components

--- src/App.js
+++ src/App.js
@@ -2,17 +2,39 @@ import logo from "./logo.svg";
 import "./App.css";
 import CheckBox from "./components/common/CheckBox";
 import todosData from "./components/const-data/todoData";
+import React from "react";
 
-function App() {
-  const todositems = todosData.map((item) => {
-    return <CheckBox key={item.id} item={item} />;
-  });
+// function App() {
+//   const todositems = todosData.map((item) => {
+//     return <CheckBox key={item.id} item={item} />;
+//   });
 
-  return (
-    <>
-      <div class="todo-list">{todositems}</div>;
-    </>
-  );
+//   return (
+//     <>
+//       <div class="todo-list">{todositems}</div>;
+//     </>
+//   );
+// }
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: todosData,
+    };
+  }
+
+  render() {
+    const todositems = this.state.todos.map((item) => {
+      return <CheckBox key={item.id} item={item} />;
+    });
+
+    return (
+      <>
+        <div class="todo-list">{todositems}</div>;
+      </>
+    );
+  }
 }
 
 export default App;
