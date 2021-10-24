01961c23ae3396674d6b6f578a5ded473a8c79fc
badr1106 <ahmed.badr_aast@hotmail.com>
2021-10-24 17:05:49 +0200

    Using Event Handling

--- src/App.js
+++ src/App.js
@@ -4,18 +4,6 @@ import CheckBox from "./components/common/CheckBox";
 import todosData from "./components/const-data/todoData";
 import React from "react";
 
-// function App() {
-//   const todositems = todosData.map((item) => {
-//     return <CheckBox key={item.id} item={item} />;
-//   });
-
-//   return (
-//     <>
-//       <div class="todo-list">{todositems}</div>;
-//     </>
-//   );
-// }
-
 class App extends React.Component {
   constructor() {
     super();
--- src/components/common/CheckBox.js
+++ src/components/common/CheckBox.js
@@ -2,11 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./CheckBox.css";
 
+function changeChecked() {
+  console.log("Changed");
+}
+
 function CheckBox(props) {
   return (
     <>
       <div className="checkbox-card">
-        <input type="checkBox" checked={props.item.completed} />
+        <input
+          type="checkBox"
+          checked={props.item.completed}
+          onChange={changeChecked}
+        />
         <h3>{props.item.text} </h3>
       </div>
     </>
